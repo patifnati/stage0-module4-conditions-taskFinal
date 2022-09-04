bfdaaa30080045122f9089fc6cfd8b0b7a537d1d
patifnati <rzezniczekpatrycja@gmail.com>
2022-09-04 22:03:52 +0200

    task done

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+      //  if (firstSide <= 0 || secondSide <= 0 || thirdSide <=0) {
 
+        if ((firstSide + thirdSide > secondSide) && (firstSide + secondSide > thirdSide) && (secondSide + thirdSide > firstSide)) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
+        }
     }
-}
